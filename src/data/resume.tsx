5941615d82da7abb4bbc29e5508de56579855cd4
Developwithayush <developwithdubey@gmail.com>
2026-07-27 01:50:14 +0530

    fixing tthings

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -4,7 +4,7 @@ import { HomeIcon } from "lucide-react";
 export const DATA = {
   name: "Ayush",
   initials: "A",
-  url: "https://ayush-fxx.vercel.app",
+  url: "https://ayush-editsfx.vercel.app",
   location: "Remote",
   locationLink: "https://google.com/maps",
   description:
